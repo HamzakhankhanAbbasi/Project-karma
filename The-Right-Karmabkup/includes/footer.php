7575f53948d28fa209fbef7dac0afe88bdb81c36
Hamza Khan <hamza.khan@technado.pk>
2022-02-12 03:27:56 +0500

    project karma

---
+++ The-Right-Karmabkup/includes/footer.php
@@ -0,0 +1,107 @@
+<footer class="footerMain">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-12 col-lg-5">
+				<div class="footer-sect">
+					<a href="#!">
+						<img src="assets/images/logo.png" class="img-fluid">
+					</a>
+					<p class="ft-paratext foot-text1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididun.</p>
+				</div>
+			</div>
+			<div class="col-12 col-md-12 col-lg-7">
+			   <div class="newsletter-sectWrap">
+                    <p class="boldTitle">Subscribe To Our Newsletter</p>
+                    <p class="small-para">Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
+                    <div class="inp-Btn-sec">
+                        <input type="email" placeholder="Email Address">
+                        <a href="#!" class="btn subscribeBtn">Subscribe</a>
+                    </div>
+                </div>
+				<div class="row">
+					<div class="col-12 col-md-4 col-lg-4">
+						<div class="footer-sect">
+							<p class="ft-Text1">Quick Links</p>
+							<ul class="footerLinks">
+								<li>
+									<a href="#!">Home</a>
+								</li>
+								<li>
+									<a href="#!">About Us</a>
+								</li>
+								<li>
+									<a href="#!">Experience</a>
+								</li>
+								<li>
+									<a href="#!">Causes</a>
+								</li>
+								<li>
+									<a href="#!">Connect</a>
+								</li>
+								<li>
+									<a href="#!">Blog</a>
+								</li>
+							</ul>
+						</div>
+				   </div>
+				    <div class="col-12 col-md-4 col-lg-4">
+						<div class="footer-sect">
+							<p class="ft-Text1">Follow Us On</p>
+							<ul class="footerLinks">
+								<li>
+									<a href="#!">Facebook</a>
+								</li>
+								<li>
+									<a href="#!">Instagram</a>
+								</li>
+								<li>
+									<a href="#!">Twitter</a>
+								</li>
+								<li>
+									<a href="#!">Youtube</a>
+								</li>
+							</ul>
+						</div>
+			        </div>
+					<div class="col-12 col-md-4 col-lg-4">
+						<div class="footer-sect">
+							<p class="ft-Text1">Contact Info</p>
+							<ul class="footerLinks">
+								<li>
+									<a href="#!">Phone:(008) 555 666 88</a>
+								</li>
+								<li>
+									<a href="#!">Email:info@example</a>
+								</li>
+							</ul>
+						</div>
+			        </div>
+				</div>
+			</div>		
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-12 col-lg-12">
+				<div class="copyRight-sect">
+					<p class="ft-copyright">©Copyright 2021  The Right Karma Investments LLC All Right Reserved</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	 <img src="assets/images/angel.png" alt="airplane" class="img-fluid angelImg">
+	 <img src="assets/images/angel2.png" alt="airplane" class="img-fluid angeltwo">
+</footer>
+
+
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/jquery-3.5.1.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/stellarnav.min.js"></script>
+<script src="assets/js/jquery.fancybox.min.js"></script> 
+<script src="assets/js/aos.js"></script>
+<script src="assets/js/custom.js"></script> 
+
+<script>
+  AOS.init();
+</script>
+</body>
+</html>
---
+++ The-Right-Karmabkup/includes/header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>The Right Karma</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="icon" href="assets/images/favicon.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="assets/css/stellarnav.min.css" />
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="assets/css/font-awesome.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="stylesheet" href="assets/css/jquery.fancybox.min.css" />
+	<link rel="stylesheet" href="assets/css/style.css" />
+	<link rel="stylesheet" href="assets/css/responsive.css" />
+	<link rel="stylesheet" href="assets/css/aos.css"/>
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="top-header">
+				<div class="mailing-links-sect mobile-info">
+					<a class="mailing-links">
+						<span><i class="fas fa-phone-alt"></i></span>
+						<p>(008) 555 666 888</p>
+					</a>
+					<a class="mailing-links">
+						<span><i class="fas fa-envelope"></i></span>
+						<p>info@example.com</p>
+					</a>
+				</div>
+				<div class="mainLogo">
+					<a href="#!">
+						<img src="assets/images/logo.png" class="img-fluid">
+					</a>
+				</div>
+				<div class="login-sect">
+					<div class="login">
+						<a href="#!">
+							<i class="fas fa-user"></i>
+						</a>
+						<p class="text-login text-logintwo">Login</p>
+					</div>
+					<a class="mailing-links">
+						<span><i class="fas fa-wallet"></i></span>
+					</a>
+				</div>
+				<div class="donateNow">
+					<a href="#!">Donate Now</a>
+				</div>
+			</div>
+		</div>
+	<div class="mobile-wrap">	
+		<div class="allNav-links customGradient">
+				<button type="button" class="toogle-close-btn">
+					<i class="fas fa-times"></i>
+				</button> 
+				<div class="container">
+					<ul class="navAnchors">
+						<li>
+							<a href="#!">Home</a>
+						</li>
+						<li>
+							<a href="#!">About The Right Karma</a>
+						</li>
+						<li>
+							<a href="#!">The Right Experience</a>
+						</li>
+						<li>
+							<a href="#!">The Right Causes</a>
+						</li>
+						<li>
+							<a href="#!">The Right Karma Connect</a>
+						</li>
+						<li>
+							<a href="#!">Enter Into The Right Karma</a>
+						</li>
+						<li>
+							<a href="#!">Contact Us</a>
+						</li>
+					</ul>
+				</div>
+		</div>
+	</div>
+		<a href="javascript:void(0)" class="mobile-toggle"><i class="fas fa-bars"></i></a>
+	</header>
+
+
+	
